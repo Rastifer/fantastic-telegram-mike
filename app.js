a888ba1febd5dcf4b1af8f9cbb5c5cc147174f47
Jacob <jacobrast02@gmail.com>
2024-01-21 20:38:05 -0600

    added port binding

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5500;
 
 app.get('/', function (req, res) {
     res.send('Hello World from Express')
 })
 
-app.listen(3000)
+// app.listen(3000)
+
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+app.listen(port, () => {
+    console.log(`Mike app listening on port ${port}`)
+})
